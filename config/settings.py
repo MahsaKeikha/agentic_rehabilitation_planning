cbf4233ab3fd5e035d8b3c324192e501b2e63c03
MahsaKeikha <mahsa.keikha@gmail.com>
2026-08-22 02:18:14 -0700

    Harden F60 safety and support layers

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,5 @@
-DEFAULTS={"require_human_review":True,"treatment_prescribing":False,"emergency_replacement":False}
+DEFAULTS = {
+    "require_human_review": True,
+    "treatment_prescribing": False,
+    "emergency_replacement": False,
+}
